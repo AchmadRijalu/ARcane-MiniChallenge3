3af0bb30bfb44b019a29dea6d2ba2542d6d7f041
Achmad Rijalu Arianindita <achmad.rijalu@gmail.com>
2023-07-28 08:33:52 +0700

    initial commit

---
+++ MagiArena-MiniChallenge3/MagiArena-MiniChallenge3/MagiArena_MiniChallenge3App.swift
@@ -0,0 +1,17 @@
+//
+//  MagiArena_MiniChallenge3App.swift
+//  MagiArena-MiniChallenge3
+//
+//  Created by Achmad Rijalu on 28/07/23.
+//
+
+import SwiftUI
+
+@main
+struct MagiArena_MiniChallenge3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
